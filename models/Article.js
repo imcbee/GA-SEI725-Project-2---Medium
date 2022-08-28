af466b4a573f363f4133b03f4870e301eeca0850
Corey Lott <Lott-c@hotmail.com>
2022-08-28 09:42:43 -0500

    Created models|connection with mongo

---
+++ models/Article.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema(
+  {
+  	author: { type: String, required: true },
+  	title: { type: String, required: true },
+  	text: {type: String, required: true },
+  },
+	{ timestamps: true });
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
---
+++ models/tempDB.js
@@ -0,0 +1,21 @@
+const articles = [
+    {
+        author: 'Corey',
+        title: 'How much wood',
+        text: 'could a woodchuck',
+    },
+    {
+        author: 'Ian',
+        title: 'chuck if',
+        text: 'a woodchuck',
+    },
+    {
+        author: 'Nsikak',
+        title: 'could',
+        text: 'chuck wood',
+    },
+];
+
+module.exports = articles;
+
+// i assume this DB was for our fake db lol. 
