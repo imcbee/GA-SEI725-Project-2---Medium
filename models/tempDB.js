46e4d21a3481dabf1f9c4d7e625622868ce952e1
Corey Lott <Lott-c@hotmail.com>
2022-08-28 16:32:37 -0500

    Update to models

--- models/tempDB.js
+++ models/tempDB.js
@@ -1,21 +1,39 @@
+require('../connection/db.connection');
+const db = require('./index');
+
 const articles = [
     {
         author: 'Corey',
         title: 'How much wood',
         text: 'could a woodchuck',
+        publishDate: new Date(2020, 09, 28),
     },
     {
         author: 'Ian',
         title: 'chuck if',
         text: 'a woodchuck',
+        publishDate: new Date(2020, 09, 27),
     },
     {
         author: 'Nsikak',
         title: 'could',
         text: 'chuck wood',
+        publishDate: new Date(2020, 09, 26),
     },
 ];
 
-module.exports = articles;
+async function reloadData() {
+	try {
+		let deleted = await db.Articles.deleteMany({});
+		console.log(deleted)
+		
+		let reloading = await db.Articles.insertMany(articles);
+		console.log(reloading)
+	} catch (err) {
+		console.log(err);
+	}
+}
 
-// i assume this DB was for our fake db lol. 
+reloadData();
+
+// module.exports = articles;
