90d3ed0c94b276e478abf72580d3b65506d32458
Ian <imcbee@terpmail.umd.edu>
2022-09-05 20:24:02 -0400

    push before main merge

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 require('dotenv').config();
 const navLinks = require('./navLinks');
+const db = require('./models');
 
 // Controller Imports
 const {articles, reviews, user} = require('./controllers');
@@ -43,8 +44,10 @@ app.use('/reviews', reviews);
 app.use('/user', user);
 
 // 404 Route
-app.get('*', (req, res) => {
-    res.render('404.ejs')
+app.get('*', async (req, res, next) => {
+    const userSession = await db.User.find(req.session.currentUser);
+    const context = {username: userSession}
+    res.render('404.ejs', context)
 });
 
 // SERVER
